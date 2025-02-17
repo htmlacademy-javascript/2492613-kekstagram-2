1ab5c477cca0989e2e906b75b558c3a507ce0933
ekaterina.likhacheva@gmail.com <ekaterina.likhacheva@gmail.com>
2025-02-17 18:06:07 +0300

    Добавляет открытие окна большой картинки - модуль 8

---
+++ js/openModal.js
@@ -0,0 +1,98 @@
+import { COMMENTS_STEP } from './constants.js';
+import { isEscapeKey } from './util.js';
+
+const body = document.querySelector('body');
+const modalElement = document.querySelector('.big-picture');
+const modalCloseElement = modalElement.querySelector('.big-picture__cancel');
+const imageElement = modalElement.querySelector('.big-picture__img img');
+const likesElement = modalElement.querySelector('.likes-count');
+const descriptionElement = modalElement.querySelector('.social__caption');
+const commentsCountElement = modalElement.querySelector('.social__comment-count');
+const commentsShownElement = commentsCountElement.querySelector('.social__comment-shown-count');
+const commentsTotalElement = commentsCountElement.querySelector('.social__comment-total-count');
+const commentsList = modalElement.querySelector('.social__comments');
+const commentsLoader = modalElement.querySelector('.social__comments-loader');
+const commentTemplate = commentsList.querySelector('.social__comment');
+
+let localComments;
+let renderedCommets = 0;
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const showModal = () => {
+  modalElement.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const renderStatistic = () => {
+  commentsShownElement.textContent = renderedCommets;
+};
+
+const renderLoader = () => {
+  if (localComments.length) {
+    commentsLoader.classList.remove('hidden');
+  } else {
+    commentsLoader.classList.add('hidden');
+  }
+};
+
+const renderComments = () => {
+  const commentsListFragment = document.createDocumentFragment();
+  localComments.splice(0, COMMENTS_STEP).forEach((comment) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    const commentImgElement = commentElement.querySelector('.social__picture');
+    commentImgElement.src = comment.avatar;
+    commentImgElement.alt = comment.name;
+    commentElement.querySelector('.social__text').textContent = comment.message;
+    commentsListFragment.appendChild(commentElement);
+
+    renderedCommets++;
+  });
+  commentsList.appendChild(commentsListFragment);
+  renderStatistic();
+  renderLoader();
+};
+
+commentsLoader.addEventListener('click', () => {
+  renderComments();
+});
+
+const renderModal = (picture) => {
+  localComments = [...picture.comments];
+  renderedCommets = 0;
+  commentsList.innerHTML = '';
+  imageElement.src = picture.url;
+  imageElement.alt = picture.description;
+  descriptionElement.textContent = picture.description;
+  likesElement.textContent = picture.likes;
+
+  commentsTotalElement.textContent = picture.comments.length;
+
+  renderComments();
+};
+
+
+export const openModal = (data) => {
+  showModal();
+  renderModal(data);
+
+};
+
+const closeModal = () => {
+  modalElement.classList.add('hidden');
+
+  body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+modalCloseElement.addEventListener('click', () => {
+  closeModal();
+});
--- js/renderPictures.js
+++ js/renderPictures.js
@@ -1,15 +1,21 @@
+import { openModal } from './openModal.js';
+
 const picturesContainer = document.querySelector('.pictures');
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const picturesFragment = document.createDocumentFragment();
+let localData;
 
 export const renderPictures = (pictures) => {
+  localData = [...pictures];
+  const picturesFragment = document.createDocumentFragment();
+
   pictures.forEach((element) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     const pictureImg = pictureElement.querySelector('.picture__img');
     pictureImg.src = element.url;
     pictureImg.alt = element.description;
+    pictureElement.dataset.pictureId = element.id;
     pictureElement.querySelector('.picture__comments').textContent = element.comments.length;
     pictureElement.querySelector('.picture__likes').textContent = element.likes;
     picturesFragment.appendChild(pictureElement);
@@ -17,3 +23,12 @@ export const renderPictures = (pictures) => {
 
   picturesContainer.appendChild(picturesFragment);
 };
+
+picturesContainer.addEventListener('click', (evt) => {
+  const pictureElement = evt.target.closest('.picture');
+  if(pictureElement) {
+    const id = Number(pictureElement.dataset.pictureId);
+    const pictureData = localData.find((item) => item.id === id);
+    openModal(pictureData);
+  }
+});
